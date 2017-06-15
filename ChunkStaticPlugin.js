55bcb7b39035900b1f1cb69abe9e9d38269ef584
marzk <c0de1768@qq.com>
2017-06-15 17:30:50 +0800

    0.0.22 handle chunkstaticmanifest parent

--- ChunkStaticPlugin.js
+++ ChunkStaticPlugin.js
@@ -3,7 +3,9 @@ const url = require('url');
 
 const staticManifest = {};
 
-function identity(i) { return i; }
+function identity(i) {
+  return i;
+}
 
 function ChunkStaticPlugin(opts) {
   opts = opts ? opts : {};
@@ -12,36 +14,53 @@ function ChunkStaticPlugin(opts) {
   this.cb = opts.cb || identity;
 }
 
-ChunkStaticPlugin.prototype.apply = function (compiler) {
+ChunkStaticPlugin.prototype.apply = function(compiler) {
   const fileName = this.fileName;
   const cache = this.cache;
   const publicPath = compiler.options.output.publicPath;
   const cb = this.cb;
-  compiler.plugin('emit', function (compilation, callback) {
+  compiler.plugin('emit', function(compilation, callback) {
     compilation.chunks.forEach(chunk => {
-      const chunkFiles = chunk.files.map(file => url.resolve(publicPath || '', file));
-      const js = chunkFiles[0];
+      const chunkFiles = chunk.files.map(file =>
+        url.resolve(publicPath || '', file)
+      );
+      const js = getFiles(chunk, file => url.resolve(publicPath || '', file));
       const css = chunkFiles.filter(file => /\.css($|\?)/.test(file));
 
       cache[chunk.name] = cb({
         js: [].concat(js),
-        css: css
+        css: css,
       });
     });
 
     const source = JSON.stringify(cache, null, 2);
 
     compilation.assets[fileName] = {
-      source: function () {
+      source: function() {
         return source;
       },
-      size: function () {
+      size: function() {
         return source.length;
-      }
+      },
     };
 
     callback();
   });
-}
+};
 
 module.exports = ChunkStaticPlugin['default'] = ChunkStaticPlugin;
+
+function getFiles(chunk, visit) {
+  if (chunk.files.length === 0) return [];
+
+  const file = visit(chunk.files[0]);
+  let files = [];
+  if (chunk.parents && chunk.parents.length) {
+    chunk.parents.forEach(chunk => {
+      files = files.concat(getFiles(chunk, visit));
+    });
+  }
+  files.push(file);
+
+  return files;
+}
