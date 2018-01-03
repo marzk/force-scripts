3d94e271440c8a738da311da9eb5ca8061849f5e
marzk <c0de1768@qq.com>
2018-01-03 11:21:30 +0800

    1.1.6 fix fs bug

--- fs.js
+++ fs.js
@@ -3,10 +3,10 @@ const forceConfig = require('./load-config')();
 let fs;
 
 if (forceConfig.isProd) {
+  fs = require('fs');
+} else {
   const mfs = require('memory-fs');
   fs = new mfs();
-} else {
-  fs = require('fs');
 }
 
 module.exports = fs;
